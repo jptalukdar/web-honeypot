2ae3bd92757a0c63d74c7b47cc066c5350188505
jptalukdar <jyotiplaban@gmail.com>
2022-01-05 11:35:35 +0530

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 yum update
-yum install python3-pip python3-dev git
-sudo amazon-linux-extras install nginx1
+yum install -y python3-pip python3-dev git
+sudo amazon-linux-extras install -y nginx1
 cd /opt/
 git clone https://github.com/jptalukdar/web-honeypot.git -- web 
 cd web
@@ -10,9 +10,6 @@ python3 -m venv venv
 source ./venv/bin/activate
 pip3 install gunicorn
 pip3 install -r requirements.txt
-cp build/webhen.service /etc/systemd/system/
+cp deploy/webhen.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start webhen
-
-
-https://github.com/jptalukdar/web-honeypot.git
